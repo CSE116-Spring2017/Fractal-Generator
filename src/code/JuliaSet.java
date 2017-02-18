e97b8ab72505cd87fdc724cd458be6f09b9d3aeb
sriniketh923 <srinikethvarma22@gmail.com>
2017-02-17 20:20:04 -0500

    JuliaSet commit

--- src/code/JuliaSet.java
+++ src/code/JuliaSet.java
@@ -1,5 +1,22 @@
 package code;
 
 public class JuliaSet {
+	
+	private int[][] _juliaFractal;
+	private double[][] _coordinates;
+	
+	public JuliaSet(){
+		_juliaFractal = new int[512][512];
+		_coordinates = new double[512][512];
+	}
+	
+	public void escapeTime(double x, double y){
+		
+	}
+	
+	public void coordinates(double x, double y){
+		double xCalc = x;
+		double yCals = y;
+	}
 
 }
