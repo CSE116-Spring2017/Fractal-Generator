7950b24ff17e0bed4d27577c5ba9e3985e97c39b
sriniketh923 <srinikethvarma22@gmail.com>
2017-02-17 20:41:43 -0500

    JuliaSet

--- src/code/JuliaSet.java
+++ src/code/JuliaSet.java
@@ -1,22 +1,30 @@
 package code;
 
 public class JuliaSet {
-	
+
 	private int[][] _juliaFractal;
 	private double[][] _coordinates;
-	
-	public JuliaSet(){
+
+	public JuliaSet() {
 		_juliaFractal = new int[512][512];
 		_coordinates = new double[512][512];
 	}
-	
-	public void escapeTime(double x, double y){
-		
+
+	public void escapeTime(double x, double y) {
+
 	}
-	
-	public void coordinates(double x, double y){
+
+	public void coordinates(double x, double y) {
 		double xCalc = x;
 		double yCals = y;
+		double distance = 0.0;
+		int passes = 0;
+
+		distance = Math.sqrt(((x - 0) * (x - 0)) + ((y - 0) * (y - 0)));
+		while (distance <= 4.0 && passes < 255) {
+
+		}
+
 	}
 
 }
