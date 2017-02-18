ec762a8adfcc8feac7b646a1036a409d96bfeebd
likhithr <likhithr@buffalo.edu>
2017-02-17 20:18:58 -0500

    test1

--- src/code/BurningShip.java
+++ src/code/BurningShip.java
@@ -1,5 +1,30 @@
 package code;
 
-public class burningShip {
+public class BurningShip {
+	
+	private int[][] _escape_time;
+	private double[][] _coordinates;
+	
+	
 
+	public BurningShip(){
+		
+		_escape_time=new int[512][512];
+		_coordinates=new double[512][512];
+		
+	}
+	
+	public void escapeTime(double x,double y)
+	{ 
+		
+		
+	}
+	public void coordinates(double x,double y)
+	{ 
+		
+		
+	}
+	
+	
+	
 }
