1c56ceacd144b32aa4154d879c89bbb808b0aed6
WenQian Zhao <wenqianz98@gmail.com>
2017-02-17 20:18:39 -0500

    1

--- src/code/MandelbrotSet.java
+++ src/code/MandelbrotSet.java
@@ -1,5 +1,20 @@
 package code;
 
 public class MandelbrotSet {
-
+	
+	private double[][] _coordinates;
+	private int[][] _finalFractal;
+	
+	public MandelbrotSet(){
+		_coordinates = new double[512][512];
+		_finalFractal = new int[512][512];
+	}
+	
+	public void escapeTime(double currentx, double currenty) {
+		
+	}
+	
+	public void coordinate(double x, double y) {
+		
+	}
 }
