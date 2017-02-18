32a1c5d2552c3a0a48e978b5696e3f83a685c069
sriniketh923 <srinikethvarma22@gmail.com>
2017-02-18 17:50:48 -0500

    Julia Set

--- src/code/JuliaSet.java
+++ src/code/JuliaSet.java
@@ -2,15 +2,27 @@ package code;
 
 public class JuliaSet {
 
-	private double[][] _juliaFractal;
-	private double[][] _coordinates;
+	private int[][] _juliaFractal;
+	private double[][] _x;
+	private double[][] _y;
 
 	public JuliaSet() {
-		_juliaFractal = new double[512][512];
-		_coordinates = new double[512][512];
+		_x = setCoordinateX();
+		_y = setCoordinateY();
+		_juliaFractal = fractals();
+	}
+	
+	public static void main(String[] args) {
+		JuliaSet m = new JuliaSet();
+		double [][] x = m.setCoordinateX();
+		double [][] y = m.setCoordinateY();
+		int [][] f = m.fractals();
+		System.out.println(x[0][0]+","+y[0][0]);
+		System.out.println("escape time: "+f[0][0]);
+		System.out.println(m.escapeTime(-1.7, -1.0));
 	}
 
-	public void escapeTime(double x, double y) {
+	public int escapeTime(double x, double y) {
 		double xCalc = x;
 		double yCalc = y;
 		double distance = 0.0;
@@ -22,42 +34,58 @@ public class JuliaSet {
 			passes = passes + 1;
 			distance = Math.sqrt(((xCalc) * (xCalc)) + ((yCalc) * (yCalc)));
 		}
-		
+		return passes;
 	}
-	
+
 	public double[][] setCoordinateX() {
 		double[][] xx = new double[512][512];
-		double dx = (3.4)/512;
+		double dx = (3.4) / 512;
 		double x = -1.7;
-		for(int row = 0; row < xx.length; row++) {
+		for (int row = 0; row < xx.length; row++) {
 			x = x + dx;
-			for(int col = 0; col < xx[row].length; col++) {
+			for (int col = 0; col < xx[row].length; col++) {
 				xx[row][col] = x;
 			}
 		}
 		return xx;
 	}
+
 	public double[][] setCoordinateY() {
 		double[][] yy = new double[512][512];
-		double dy = (2.0)/512;
-		for(int row = 0; row < yy.length; row++) {
+		double dy = (2.0) / 512;
+		for (int row = 0; row < yy.length; row++) {
 			double y = -1.0;
-			for(int col = 0; col < yy[row].length; col++) {
+			for (int col = 0; col < yy[row].length; col++) {
 				y = y + dy;
 				yy[row][col] = y;
 			}
 		}
 		return yy;
 	}
-	
+
+	public double xCoordinate(int x, int y) {
+		return _x[x][y];
+	}
+
+	public double yCoordinate(int x, int y) {
+		return _y[x][y];
+	}
 
 	public void update(double x, double y) {
-		double x1 = 0.0;
-		double y1 = 0.0;
 		if (x >= -1.7 && x <= 1.7 && x >= -1.0 && x <= 1.0) {
-			x1 = ((x * x) - (y * y)) + (-0.72689);
-			y1 = (2 * x * y) + 0.188887;
+			double x1 = ((x * x) - (y * y)) + (-0.72689);
+			double y1 = (2 * x * y) + 0.188887;
 		}
+	}
+
+	public int[][] fractals() {
+		 int[][] result = new int[512][512];
+		 for(int row = 0; row < result.length; row=row+1) {
+			 for(int col = 0; col < result[row].length; col=col+1) {
+				 result[row][col] = escapeTime(_x[row][col],_y[row][col]);
+			 }
+		 } 
+		 return result;
 	}
 
 }
