1e43be17334c8881acdb93b142b482259a8368ef
WenQian Zhao <wenqianz98@gmail.com>
2017-02-17 21:13:54 -0500

    1

--- src/code/MandelbrotSet.java
+++ src/code/MandelbrotSet.java
@@ -10,20 +10,8 @@ public class MandelbrotSet {
 		_finalFractal = new int[512][512];
 	}
 
-	public int escapeTime(double currentx, double currenty) {
-		double xCalc = currentx;
-		double yCalc = currenty;
-		
-		double dist = Math.sqrt((xCalc * xCalc) + (yCalc * yCalc));
-		int passes = 0;
-		while(dist <= 4 && passes<255) {
-			xCalc = (xCalc*xCalc) - (yCalc*yCalc) + currentx;
-			yCalc = 2 * xCalc * yCalc + currenty;
-			passes ++;
-			dist = Math.sqrt((xCalc * xCalc) + (yCalc * yCalc));
-		}
-		
-		return passes;
+	public void escapeTime(double currentx, double currenty) {
+	
 	}
 
 	public void coordinate(double x, double y) {
