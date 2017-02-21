3cdaf62a2643836cee1775621e5baa2be6eb2922
SaleemAs4 <Saleemas@buffalo.edu>
2017-02-21 01:29:45 -0500

    MultibrotSet Push

--- src/code/MultibrotSet.java
+++ src/code/MultibrotSet.java
@@ -2,76 +2,67 @@ package code;
 
 public class MultibrotSet {
 
-
-	private int[][] _multiFractal;
 	private double[][] _x;
 	private double[][] _y;
+	private int[][] _finalFractal;
 
 	public MultibrotSet() {
 		_x = setCoordinateX();
 		_y = setCoordinateY();
-		_multiFractal = fractals();
+		_finalFractal = fractals();
+		
 	}
-	
 	public static void main(String[] args) {
 		MultibrotSet m = new MultibrotSet();
 		double [][] x = m.setCoordinateX();
 		double [][] y = m.setCoordinateY();
 		int [][] f = m.fractals();
-		System.out.println(x[0][0]+","+y[0][0]);
-		System.out.println("escape time: "+f[0][0]);
+		
 		System.out.println(m.escapeTime(0.9921875, 1.05625));
+		
 	}
-
-	public int escapeTime(double x, double y) {
-		double xCalc = x;
-		double yCalc = y;
-		double distance = 0.0;
-		int passes = 0;
-
-		distance = Math.sqrt(((xCalc) * (xCalc)) + ((yCalc) * (yCalc)));
-		while (distance <= 2.0 && passes < 255) {
-			double xtemp = xCalc;
-			xCalc = ((x * x) - (y * y)) + (-0.72689);
-			yCalc = (2 * x * y) + 0.188887;
-			passes = passes + 1;
-			distance = Math.sqrt(((xCalc) * (xCalc)) + ((yCalc) * (yCalc)));
-		}
-		return passes;
-	}
-
+	
 	public double[][] setCoordinateX() {
 		double[][] xx = new double[512][512];
-		double dx = (3.4) / 512;
-		double x = -1.7;
-		for (int row = 0; row < xx.length; row++) {
-			x = x + dx;
-			for (int col = 0; col < xx[row].length; col++) {
+		double dx = (1+1)/512;
+		double x = -1;
+		for(int row = 0; row < xx.length; row++) {
+			for(int col = 0; col < xx[row].length; col++) {
 				xx[row][col] = x;
 			}
+			x = x + dx;
 		}
 		return xx;
 	}
-
 	public double[][] setCoordinateY() {
 		double[][] yy = new double[512][512];
-		double dy = (2.0) / 512;
-		for (int row = 0; row < yy.length; row++) {
-			double y = -1.0;
-			for (int col = 0; col < yy[row].length; col++) {
-				y = y + dy;
+		double dy = (1.3+1.3)/512;
+		for(int row = 0; row < yy.length; row++) {
+			double y = -1.3;
+			for(int col = 0; col < yy[row].length; col++) {
 				yy[row][col] = y;
+				y = y + dy;
 			}
 		}
 		return yy;
 	}
 
-	public double xCoordinate(int x, int y) {
-		return _x[x][y];
-	}
-
-	public double yCoordinate(int x, int y) {
-		return _y[x][y];
+	
+	public int escapeTime(double currentx, double currenty) {
+		double xCalc = currentx;
+		double yCalc = currenty;
+		double dist = Math.sqrt((xCalc * xCalc) + (yCalc * yCalc));
+		int passes = 0;
+		
+		while(dist <= 2 && passes<255) {
+			double xtemp = xCalc;
+			xCalc = (xCalc*xCalc*xCalc) - (3*yCalc*yCalc*xCalc) + currentx;
+			yCalc = (3*xtemp*xtemp*yCalc)-(yCalc*yCalc*yCalc) + currenty;
+			passes = passes + 1;
+			dist = Math.sqrt((xCalc * xCalc) + (yCalc * yCalc));
+		}
+		
+		return passes;
 	}
 	
 	public int[][] fractals() {
