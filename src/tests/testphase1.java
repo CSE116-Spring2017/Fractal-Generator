47826e1a85b98814aed9a619a4173913f0969496
likhithr <likhithr@buffalo.edu>
2017-02-20 19:46:30 -0500

    lit12314

--- src/tests/testphase1.java
+++ src/tests/testphase1.java
@@ -3,12 +3,11 @@ package tests;
 import static org.junit.Assert.*;
 
 import javax.swing.plaf.synth.SynthSeparatorUI;
-import static org.junit.Assert.assertNotEquals;
-
 
 import org.junit.Test;
 
 import code.BurningShip;
+import code.JuliaSet;
 import code.MandelbrotSet;
 
 public class testphase1 {
@@ -24,6 +23,7 @@ public class testphase1 {
 		BurningShip ship = new BurningShip();
 		assertEquals(255, ship.escapeTime(-1.7443359374999874, -0.017451171875000338));
 	}
+
 	@Test
 	public void BurningShipTest2() {
 		BurningShip ship = new BurningShip();
@@ -40,21 +40,48 @@ public class testphase1 {
 			for (int col = 0; col < result[row].length; col = col + 1) {
 				result[row][col] = ship.escapeTime(_x[row][col], _y[row][col]);
 
-				assertNotEquals(1,result[row][col]);
-				assertNotEquals(0,result[row][col]);
-				
-				
+				assertNotEquals(1, result[row][col]);
+				assertNotEquals(0, result[row][col]);
+
 			}
 		}
 
 	}
 
 	@Test
-	public void test() {
+	public void BurningShipFractalSize() {
+		BurningShip ship = new BurningShip();
 
-		System.out.println("Lit");
-		// Okay
+		int[][] result = ship.fractals();
+
+		assertEquals(512, result.length);
+		assertEquals(512, result[0].length);
 
 	}
+
+	@Test
+	public void neverExceedEscapeDistance() {
+		JuliaSet js = new JuliaSet();
+		assertEquals(255, js.escapeTime(1.0492187499999897, -0.234375));
+	}
+
+	@Test
+	public void ExceedEscapeDistance() {
+		JuliaSet js = new JuliaSet();
+		assertEquals(1, js.escapeTime(1.6933593749999853, 0.9765625));
+	}
+	@Test
+	public void JuliaSetFractalSize()
+	{ 
+		JuliaSet js = new JuliaSet();
+		
+		int[][] result = js.fractals();
+
+		assertEquals(512, result.length);
+		assertEquals(512, result[0].length);
+		
+		
+	}
+
 
 }
