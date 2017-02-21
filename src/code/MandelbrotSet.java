2dfb4205ffe36218f98f2ed1a85e3dae86bac171
WenQian Zhao <wenqianz98@gmail.com>
2017-02-20 20:44:37 -0500

    1

--- src/code/MandelbrotSet.java
+++ src/code/MandelbrotSet.java
@@ -1,78 +1,125 @@
 package code;
 
-public class MandelbrotSet {
+/**
+ * Class which calculates the Escape-Time Algorithms for Mandelbrot Set with
+ * X-Coordinate range from -2.15 to 0.6 Y-Coordinate range from -1.3 to 1.3
+ *
+ * @author Wenqian Zhao
+ */
 
+public class MandelbrotSet {
+	/** Array of all the x-coordinate */
 	private double[][] _x;
+	/** Array of all the y-coordinate */
 	private double[][] _y;
+	/** Array of all the escape time for each of coordinate pairs */
 	private int[][] _finalFractal;
 
+	/**
+	 * Create a Mandelbrot Set with array of x coordinate range from -2.15 to
+	 * 0.6 with 512 equally-spaced array of y coordinate range from -1.3 to 1.3
+	 * with 512 equally-spaced array of escape-time for each of 262144 pairs
+	 */
 	public MandelbrotSet() {
 		_x = setCoordinateX();
 		_y = setCoordinateY();
 		_finalFractal = fractals();
-		
+
 	}
-	public static void main(String[] args) {
-		MandelbrotSet m = new MandelbrotSet();
-		double [][] x = m.setCoordinateX();
-		double [][] y = m.setCoordinateY();
-		int [][] f = m.fractals();
-		
-		System.out.println(m.escapeTime(0.5946289062500001, 1.2949218750000122));
-		
-	}
-	
+
+	/**
+	 *  Set up all the x according to Cartesian plane
+	 * return a 2-d array of double with 512 rows and 512 columns
+	 * find the change of x 
+	 * first double is the start of the x range which is -2.15
+	 * x + change of x when row increase by one
+	 * increase of column does not effect anything
+	 * @return 2-d array of double
+	 */
 	public double[][] setCoordinateX() {
 		double[][] xx = new double[512][512];
-		double dx = (2.15+0.6)/512;
+		double dx = (2.15 + 0.6) / 512;
 		double x = -2.15;
-		for(int row = 0; row < xx.length; row++) {
-			for(int col = 0; col < xx[row].length; col++) {
+		for (int row = 0; row < xx.length; row++) {
+			for (int col = 0; col < xx[row].length; col++) {
 				xx[row][col] = x;
 			}
 			x = x + dx;
 		}
 		return xx;
 	}
+	
+	/**
+	 *  Set up all the y according to Cartesian plane
+	 * return a 2-d array of double with 512 rows and 512 columns
+	 * find the change of y
+	 * first double is the start of the x range which is -1.3
+	 * y + change of y when column increase by one
+	 * increase of row does not effect anything
+	 * @return 2-d array of double
+	 */
+
 	public double[][] setCoordinateY() {
 		double[][] yy = new double[512][512];
-		double dy = (1.3+1.3)/512;
-		for(int row = 0; row < yy.length; row++) {
+		double dy = (1.3 + 1.3) / 512;
+		for (int row = 0; row < yy.length; row++) {
 			double y = -1.3;
-			for(int col = 0; col < yy[row].length; col++) {
+			for (int col = 0; col < yy[row].length; col++) {
 				yy[row][col] = y;
 				y = y + dy;
 			}
 		}
 		return yy;
 	}
+	/**
+	   * return int of escape-time by
+	   * Set xCalc = {@code currentx}
+       * Set yCalc = {@code currenty}
+       * Set dist = distance from the point (xCalc, yCalc) to (0,0) using the Pythagorean theorem
+       * Set passes = 0
+       * While dist ≤ 2 and passes < 255 do
+       * Update xCalc and yCalc equal using the update function defined for the fractal being generated
+       * Update by using
+       * x' = x² - y² + current point's x-coordinate
+	   * y' = 2 * x * y + current point's y-coordinate
+       * Increase passes by 1
+       * Set dist distance from the point (xCalc, yCalc) to (0,0) using the Pythagorean theorem
+       * EndWhile
+	   *
+	   * @param double currentx  (x coordinate for which we will search)
+	   * @param double currenty  (y coordinate for which we will search)
+	   * @return int of escape-time for coordinate ({@code currentx} , {@code currenty})
+	   */
 
-	
 	public int escapeTime(double currentx, double currenty) {
 		double xCalc = currentx;
 		double yCalc = currenty;
 		double dist = Math.sqrt((xCalc * xCalc) + (yCalc * yCalc));
 		int passes = 0;
-		
-		while(dist <= 2 && passes<255) {
+
+		while (dist <= 2 && passes < 255) {
 			double xtemp = xCalc;
-			xCalc = (xCalc*xCalc) - (yCalc*yCalc) + currentx;
+			xCalc = (xCalc * xCalc) - (yCalc * yCalc) + currentx;
 			yCalc = 2 * xtemp * yCalc + currenty;
 			passes = passes + 1;
 			dist = Math.sqrt((xCalc * xCalc) + (yCalc * yCalc));
 		}
-		
+
 		return passes;
 	}
-	
+	/**
+	 * return 2-d array of escape-time for each of these 262144 coordinate pairs
+	 * @return 2-d array of double
+	 */
+
 	public int[][] fractals() {
-		 int[][] result = new int[512][512];
-		 for(int row = 0; row < result.length; row=row+1) {
-			 for(int col = 0; col < result[row].length; col=col+1) {
-				 result[row][col] = escapeTime(_x[row][col],_y[row][col]);
-			 }
-		 } 
-		 return result;
+		int[][] result = new int[512][512];
+		for (int row = 0; row < result.length; row = row + 1) {
+			for (int col = 0; col < result[row].length; col = col + 1) {
+				result[row][col] = escapeTime(_x[row][col], _y[row][col]);
+			}
+		}
+		return result;
 	}
 
 }
