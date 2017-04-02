ad51fa4ce3ee74fbda27bfb21588adcfc12bff64
likhithr <likhithr@buffalo.edu>
2017-04-02 16:43:11 -0400

    kir

--- src/ui/MouseHandler.java
+++ src/ui/MouseHandler.java
@@ -2,53 +2,34 @@ package ui;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 import code.Model;
 
-public class MouseHandler implements MouseListener{
+public class MouseHandler implements MouseMotionListener{
 	private Model _model;
 	
-	private int _x;
-	private int _xx;
+	private double _x;
+	private double _xx;
 	
-	private int _y;
-	private int _yy;
+	private double _y;
+	private double _yy;
 	
 	public MouseHandler(Model model){
 		_model = model;
 	}
 
 	@Override
-	public void mouseClicked(MouseEvent e) {
+	public void mouseDragged(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent e) {
+	public void mouseMoved(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		_x = e.getX();
-		_y = e.getY();
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		_xx = e.getX();
-		_yy = e.getY();
-		
-		_model.setX(_x,_xx);
-		_model.setY(_y,_yy);
-	}
-
+	
 }
